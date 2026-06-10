10226add16ce3908da06d5fe647a6f806d7fe3d5
Rohit Rai <152757435+rohitrai94@users.noreply.github.com>
2026-06-10 16:27:06 +0530

    Time: 31 ms (66.87%), Space: 137.4 MB (18.44%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.java
@@ -1,3 +1,23 @@
+// class Solution {
+//     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
+//         PriorityQueue<int[]> p = new PriorityQueue<>((a,b)->Integer.compare(a[0],b[0]));
+//         List<List<Integer>> res = new ArrayList<>();
+//         for(int i=0; i<Math.min(nums1.length, k); i++){
+//             p.add(new int[]{nums1[i], nums2[0], 0});
+//         }
+//         while(k-- > 0 && !p.isEmpty()){
+//             int[] curr = p.poll();
+//             res.add(Arrays.asList(curr[0], curr[1]));
+//             int idx = curr[2];
+//             if(idx+1 < nums2.length){
+//                 p.add(new int[]{curr[0], nums2[idx+1], idx+1});
+//             }
+//         }
+//     return res;
+//     }
+// }
+
+
 class Solution {
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
         PriorityQueue<int[]> p = new PriorityQueue<>((a, b) -> (a[0] + a[1]) - (b[0] + b[1]));
