cc4971796cf5284fdd7ba657c2bbee77f3a68642
Rohit Rai <152757435+rohitrai94@users.noreply.github.com>
2026-06-14 12:37:24 +0530

    Time: 81 ms (84%), Space: 46.4 MB (61.91%) - LeetHub

--- 2523-closest-prime-numbers-in-range/2523-closest-prime-numbers-in-range.java
+++ 2523-closest-prime-numbers-in-range/2523-closest-prime-numbers-in-range.java
@@ -1,39 +1,37 @@
 class Solution {
     public int[] closestPrimes(int left, int right) {
-        int arr[] = new int[2];
-        if(right <= 2){
-            return new int[]{-1,-1};
+        int arr[] = { -1, -1 };
+        if (right <= 2) {
+            return arr;
         }
 
-        boolean[] isPrime = new boolean[right+1];
+        boolean[] isPrime = new boolean[right + 1];
         Arrays.fill(isPrime, true);
         isPrime[0] = false;
         isPrime[1] = false;
-        for(int i=2; i*i<=right; i++){
-            if(isPrime[i]){
-                for(int j=i*i; j<=right; j += i){
-                    isPrime[j]= false;
+        for (int i = 2; i * i <= right; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= right; j += i) {
+                    isPrime[j] = false;
                 }
             }
         }
 
-        List<Integer> list = new ArrayList<>();
-
-        for(int i=left; i<=right; i++){
-            if(isPrime[i]){
-                list.add(i);
+        int prev = -1;
+        int min = right + 1;
+        for (int i = left; i <= right; i++) {
+            if (isPrime[i]) {
+                if (prev != -1) {
+                    int diff = i - prev;
+                    if (min > diff) {
+                        min = diff;
+                        arr[0] = prev;
+                        arr[1] = i;
+                    }
+                }
+                prev = i;
             }
-        }
 
-        if(list.size() <= 1) return new int[]{-1,-1};
-        int min = right+1;
-        for(int i=0; i<list.size()-1; i++){
-            int diff = list.get(i+1) - list.get(i);
-            if(min > diff){
-                arr[0] = list.get(i);
-                arr[1] = list.get(i+1);
-            }
-            min = Math.min(min, diff);
         }
         return arr;
     }
