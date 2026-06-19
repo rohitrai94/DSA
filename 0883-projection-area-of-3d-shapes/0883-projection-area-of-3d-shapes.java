7750ecd2512577a68915fb32ccd1424d5897ae6e
Rohit Rai <152757435+rohitrai94@users.noreply.github.com>
2026-06-19 11:57:11 +0530

    Time: 2 ms (94.38%), Space: 46.5 MB (9.88%) - LeetHub

--- 0883-projection-area-of-3d-shapes/0883-projection-area-of-3d-shapes.java
+++ 0883-projection-area-of-3d-shapes/0883-projection-area-of-3d-shapes.java
@@ -4,30 +4,20 @@ class Solution {
         int m = grid[0].length;
 
         int topView = 0;
+        int frontView = 0;
+        int sideView = 0;
         for(int i=0; i < n; i++){
+            int rowMax = 0;
+            int collMax = 0;
             for(int j=0; j<m; j++){
                if(grid[i][j] > 0){
                 topView++;
                }
+                rowMax = Math.max(rowMax,grid[i][j]);
+                collMax = Math.max(collMax, grid[j][i]);
             }
-        }
-
-        int frontView = 0;
-        for(int i=0; i < n; i++){
-            int max = Integer.MIN_VALUE;
-            for(int j=0; j<m; j++){
-                max = Math.max(max,grid[i][j]);
-            }
-            frontView += max;
-        }
-
-        int sideView = 0;
-        for(int i=0; i < m; i++){
-            int max = Integer.MIN_VALUE;
-            for(int j=0; j<n; j++){
-                max = Math.max(max,grid[j][i]);
-            }
-            sideView += max;
+            frontView += rowMax;
+            sideView += collMax;
         }
 
     return topView + frontView + sideView;
