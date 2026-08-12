4f88d1d4e1da0d7f10869a3590f7fffbe188cbe2
Rohit Rai <152757435+rohitrai94@users.noreply.github.com>
2026-08-12 16:22:02 +0530

    Time: 2 ms (99.64%), Space: 43.7 MB (99.71%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] count = new int[26];
+
+        for (char ch : s.toCharArray()){
+            count[ch - 'a']++; 
+        }
+        for (char ch : t.toCharArray()){
+            count[ch-'a']--;
+        }
+        for(int i=0; i<26; i++){
+            if(count[i] != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
