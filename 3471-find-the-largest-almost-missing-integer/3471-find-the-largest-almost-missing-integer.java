0941be49c1d9ecfc5b476ee98ff163af5c8d4fef
Rohit Rai <152757435+rohitrai94@users.noreply.github.com>
2026-08-18 15:47:33 +0530

    Time: 1 ms (99.3%), Space: 44.6 MB (74.74%) - LeetHub

--- 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.java
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.java
@@ -1,32 +1,33 @@
 class Solution {
-
     public int largestInteger(int[] nums, int k) {
         int n = nums.length;
-        if (n == k) {
-            int res = nums[0];
-            for (int x : nums) {
-                res = Math.max(res, x);
+        if(n == 1) return nums[0];        
+        if(n == k){
+            int max = -1;
+            for(int i=0; i<n; i++){
+            max = Math.max(max, nums[i]);
             }
-            return res;
+            return max;
         }
-        int[] count = new int[51];
-        for (int x : nums) {
-            count[x]++;
+        int arr[] = new int[51];
+        for(int i=0; i<n; i++){
+            arr[nums[i]]++;
         }
-        if (k == 1) {
-            for (int i = 50; i >= 0; --i) {
-                if (count[i] == 1) {
-                    return i;
-                }
+
+        if(k == 1){
+            for(int i= 50; i>0; i--){
+                if(arr[i] == 1) return i;
             }
             return -1;
         }
+        int st= nums[0];
+        int end = nums[n-1];
         int res = -1;
-        if (count[nums[0]] == 1) {
-            res = Math.max(res, nums[0]);
+        if(arr[st] == 1){
+            res = Math.max(res,st);
         }
-        if (count[nums[n - 1]] == 1) {
-            res = Math.max(res, nums[n - 1]);
+        if(arr[end] == 1){
+            res = Math.max(res,end);
         }
         return res;
     }
